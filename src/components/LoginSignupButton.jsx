71449c903f2bd81d7247f973339365f4eeff7111
7ussen2del <helwaly75@gmail.com>
2025-04-21 00:27:29 +0200

    Create Recovery Code

--- src/components/LoginSignupButton.jsx
+++ src/components/LoginSignupButton.jsx
@@ -1,6 +1,7 @@
-function LoginSignupButton({ children, type }) {
+function LoginSignupButton({ children, type ,onClick}) {
   return (
     <button
+    onClick={onClick}
       type={type}
       className="bg-main-750 w-full  text-white p-3 rounded-lg hover:bg-green-600 transition cursor-pointer font-bold text-2xl"
     >
