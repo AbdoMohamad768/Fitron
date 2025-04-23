fabefb981e4c0e53eb7b78723216ba6999a94c8d
omar karim amer <s2023.OmarKarim@cis.dmu.edu.eg>
2025-04-23 22:34:49 +0200

    SideBar

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
+
 function AppLayout() {
   return (
-    <div>
-      <div>Sidebar</div>
-      <div>Header</div>
-      <div>Main</div>
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="flex-1 p-4 bg-grey-50 overflow-auto">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
