f71c1b57a5e034edabf1229182a308968ad4259e
Gehad Omar <gehadansary936@gmail.com>
2025-04-27 23:23:39 +0300

    add ProfileDisplay page

--- src/pages/ProfileDisplay.jsx
+++ src/pages/ProfileDisplay.jsx
@@ -1,8 +1,52 @@
+import { useState } from "react";
+
 function ProfileDisplay() {
   // Make the display section in the profile page and make responsive to min-width 320px
-
   // You can go to the page in this rout "/app/profile/display"
-  return <div>Profile Display</div>;
+  const [measurmentData , setData] = useState({
+    measurment:"",
+    Zone:""
+  })
+  const changeDate = (e) => {
+    const { name, value } = e.target;
+    setData((e) => ({ ...e, [name]: value }));
+  };
+  return (
+    <div className="flex items-center flex-col">
+     
+      <div className="w-full  flex justify-center flex-col items-start p-6 rounded-2xl  bg-[#E8F0E8]"  >
+        
+        <div className="mb-10 flex flex-col">
+          <label  className="font-semibold mb-2">
+            Measurement Units
+          </label>
+          <select name="measurment" id="measurment" className=" w-full md:w-[600px]   sm:min-w-[320px]  bg-white pt-2 pb-2 pl-3" onChange={changeDate} value={measurmentData.measurment}>
+            <option value="">Metric (kg/m/kcal/c°)</option>
+            <option value="kg">kg</option>
+            <option value="m">m</option>
+            <option value="kcal">kcal</option>
+            <option value="c°">c°</option>
+          </select>
+        </div>
+        <div className="flex flex-col mb-10" >
+          <label  className="font-semibold mb-2">Time Zone</label>
+          <select name="Zone" id="zone" className=" md:w-[600px]   sm:w-[320px] bg-white pt-2 pb-2 pl-3" onChange={changeDate} value={measurmentData.Zone}>
+            <option value="Egypt">Egypt</option>
+            <option value="KSA">KSA</option>
+            <option value="UAE">UAE</option>
+            <option value="Jordon">Jordon</option>
+          </select>
+        </div>
+       <button
+          className="bg-main-700 pt-2 pb-2 pr-4  md:w-1/6 pl-4  sm:w-full rounded-2xl text-white"
+          type="submit"
+        >
+          Save
+        </button>
+       
+      </div>
+    </div>
+  );
 }
 
 export default ProfileDisplay;
