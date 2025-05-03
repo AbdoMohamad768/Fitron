89867264ec05e7aec631bd7d29daea11f59fbe9d
omar karim amer <s2023.OmarKarim@cis.dmu.edu.eg>
2025-05-03 21:36:13 +0300

    repair superbase.js

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -4,9 +4,9 @@ const SUPABASE_URL = "https://zbslaiojfxrfihxtjwnq.supabase.co";
 const SUPABASE_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpic2xhaW9qZnhyZmloeHRqd25xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExMzI0MDUsImV4cCI6MjA1NjcwODQwNX0.iWzBK1eYAHiAUA1mjOxqv3O6upD5jMbcFPkZghNM-JM";
 
-// export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+// const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
+// const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-export const supabase = createClient(supabaseURL, supabaseKey);
+// export const supabase = createClient(supabaseURL, supabaseKey);
